ac9122e275089d0c6e32d6a05672b9bca6b8d30c
Jarred Parr <jparr721@gmail.com>
2017-02-26 16:29:01 -0500

    eh

--- src/MoneyManager/GUI/MainMenuController.java
+++ src/MoneyManager/GUI/MainMenuController.java
@@ -1,7 +1,30 @@
 package MoneyManager.GUI;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by Jarred on 1/21/17.
  */
-public class MainMenuController {
+public class MainMenuController implements Initializable{
+
+    @FXML
+    private TableView<String> goalTable;
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    public void handleNewBudget() {
+
+    }
 }
